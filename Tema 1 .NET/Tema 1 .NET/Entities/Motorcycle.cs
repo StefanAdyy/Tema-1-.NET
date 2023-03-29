9a0c7702345b0effbc654b36e4f54de31c065958
StefanAdyy <stefanmorariu@rocketmail.com>
2023-03-29 17:46:27 +0300

    Added Motorcycle Entity

---
+++ Tema 1 .NET/Tema 1 .NET/Entities/Motorcycle.cs
@@ -0,0 +1,14 @@
+﻿namespace Tema_1_.NET.Entities
+{
+    public class Motorcycle
+    {
+        public int Id { get; set; }
+        public int ManufacturerId { get; set; }
+        public string Model { get; set; } = String.Empty;
+        public int Year { get; set; }
+        public int HorsePower { get; set; }
+        public int Torque { get; set; }
+        public int EngineDisplacement { get; set; }
+        public int TankCapacity { get; set; }
+    }
+}
