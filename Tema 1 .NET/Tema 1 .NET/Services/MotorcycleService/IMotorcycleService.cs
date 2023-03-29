d9fd111d7a11291366621ab7ca234d28f3b7659f
StefanAdyy <stefanmorariu@rocketmail.com>
2023-03-29 18:59:31 +0300

    Added Motorcycle Service

---
+++ Tema 1 .NET/Tema 1 .NET/Services/MotorcycleService/IMotorcycleService.cs
@@ -0,0 +1,13 @@
+﻿using Tema_1_.NET.Entities;
+
+namespace Tema_1_.NET.Services.MotorcycleService
+{
+    public interface IMotorcycleService
+    {
+        List<Motorcycle> GetAllMotorcycles();
+        Motorcycle? GetMotorcycleById(int id);
+        List<Motorcycle> AddMotorcycle(Motorcycle motorcycle);
+        List<Motorcycle>? UpdateHero(Motorcycle request);
+        List<Motorcycle>? DeleteMotorcycle(int id);
+    }
+}
---
+++ Tema 1 .NET/Tema 1 .NET/Services/MotorcycleService/MotorcycleService.cs
@@ -0,0 +1,89 @@
+﻿using Tema_1_.NET.Entities;
+
+namespace Tema_1_.NET.Services.MotorcycleService
+{
+    public class MotorcycleService : IMotorcycleService
+    {
+         private static List<Motorcycle> motorcycleList = new List<Motorcycle>
+        {
+            new Motorcycle
+                {
+                    Id = 1,
+                    ManufacturerId = 1,
+                    Model = "GSX-R 1000",
+                    Year = 2009,
+                    HorsePower = 185,
+                    Torque = 117,
+                    EngineDisplacement = 999,
+                    TankCapacity = 17
+                },
+                new Motorcycle
+                {
+                    Id = 2,
+                    ManufacturerId = 1,
+                    Model = "GS 500 F",
+                    Year = 2006,
+                    HorsePower = 47,
+                    Torque = 41,
+                    EngineDisplacement = 487,
+                    TankCapacity = 20
+                }
+        };
+
+        public List<Motorcycle> AddMotorcycle(Motorcycle motorcycle)
+        {
+            motorcycleList.Add(motorcycle);
+            return motorcycleList;
+        }
+
+        public List<Motorcycle>? DeleteMotorcycle(int id)
+        {
+            var motorcycle = motorcycleList.Find(x => x.Id == id);
+
+            if (motorcycle is null)
+            {
+                return null;
+            }
+
+            motorcycleList.Remove(motorcycle);
+            return motorcycleList;
+        }
+
+        public List<Motorcycle> GetAllMotorcycles()
+        {
+            return motorcycleList;
+        }
+
+        public Motorcycle? GetMotorcycleById(int id)
+        {
+            var motorcycle = motorcycleList.Find(x => x.Id == id);
+
+            if (motorcycle is null)
+            {
+                return null;
+            }
+
+            return motorcycle;
+        }
+
+        public List<Motorcycle>? UpdateHero(Motorcycle request)
+        {
+            var motorcycle = motorcycleList.Find(x => x.Id == request.Id);
+
+            if (motorcycle is null)
+            {
+                return null;
+            }
+
+            motorcycle.ManufacturerId = request.ManufacturerId;
+            motorcycle.Model = request.Model;
+            motorcycle.Year = request.Year;
+            motorcycle.HorsePower = request.HorsePower;
+            motorcycle.Torque = request.Torque;
+            motorcycle.EngineDisplacement = request.EngineDisplacement;
+            motorcycle.TankCapacity = request.TankCapacity;
+
+            return motorcycleList;
+        }
+    }
+}
