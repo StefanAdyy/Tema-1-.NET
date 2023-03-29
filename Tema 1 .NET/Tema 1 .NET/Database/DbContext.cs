8b3a8688609b7705e81befea440a2fad5baa9614
StefanAdyy <stefanmorariu@rocketmail.com>
2023-03-29 21:07:50 +0300

    Added DbContext

---
+++ Tema 1 .NET/Tema 1 .NET/Database/DbContext.cs
@@ -0,0 +1,33 @@
+﻿using Tema_1_.NET.Entities;
+
+namespace Tema_1_.NET.Database
+{
+    public static class DbContext
+    {
+        public static List<Motorcycle> Motorcycles = new List<Motorcycle>
+        {
+            new Motorcycle
+                {
+                    Id = 1,
+                    ManufacturerId = 1,
+                    Model = "GSX-R 1000",
+                    Year = 2009,
+                    HorsePower = 185,
+                    Torque = 117,
+                    EngineDisplacement = 999,
+                    TankCapacity = 17
+                },
+                new Motorcycle
+                {
+                    Id = 2,
+                    ManufacturerId = 1,
+                    Model = "GS 500 F",
+                    Year = 2006,
+                    HorsePower = 47,
+                    Torque = 41,
+                    EngineDisplacement = 487,
+                    TankCapacity = 20
+                }
+        };
+    }
+}
