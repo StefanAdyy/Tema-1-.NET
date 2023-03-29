1825c4071b55c8a4713fc3b5619814c485a70b20
StefanAdyy <stefanmorariu@rocketmail.com>
2023-03-29 19:00:07 +0300

    Refactored Motorcycle Controller
    -used the services

--- Tema 1 .NET/Tema 1 .NET/Controllers/MotorcycleController.cs
+++ Tema 1 .NET/Tema 1 .NET/Controllers/MotorcycleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Tema_1_.NET.Entities;
+using Tema_1_.NET.Services.MotorcycleService;
 
 namespace Tema_1_.NET.Controllers
 {
@@ -8,91 +9,62 @@ namespace Tema_1_.NET.Controllers
     [ApiController]
     public class MotorcycleController : ControllerBase
     {
-        private static List<Motorcycle> motorcycleList = new List<Motorcycle>
+        private readonly IMotorcycleService _motorcycleService;
+        public MotorcycleController(IMotorcycleService motorcycleService)
         {
-            new Motorcycle
-                {
-                    Id = 1,
-                    ManufacturerId = 1,
-                    Model = "GSX-R 1000",
-                    Year = 2009,
-                    HorsePower = 185,
-                    Torque = 117,
-                    EngineDisplacement = 999,
-                    TankCapacity = 17
-                },
-                new Motorcycle
-                {
-                    Id = 2,
-                    ManufacturerId = 1,
-                    Model = "GS 500 F",
-                    Year = 2006,
-                    HorsePower = 47,
-                    Torque = 41,
-                    EngineDisplacement = 487,
-                    TankCapacity = 20
-                }
-        };
-        
+            _motorcycleService = motorcycleService;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Motorcycle>>> GetAllMotorcycles()
         {
-            return Ok(motorcycleList);
+            return _motorcycleService.GetAllMotorcycles();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Motorcycle>> GetMotorcycleById(int id)
         {
-            var motorcycle = motorcycleList.Find(x => x.Id == id);
+            var result = _motorcycleService.GetMotorcycleById(id);
 
-            if(motorcycle is null)
+            if(result is null)
             {
                 return NotFound("Sorry, this motorcycle doesn't exist");
             }
 
-            return Ok(motorcycle);
+            return Ok(result);
         }
 
         [HttpPost]
         public async Task<ActionResult<List<Motorcycle>>> AddMotorcycle(Motorcycle motorcycle)
         {
-            motorcycleList.Add(motorcycle);
-            return Ok(motorcycleList);
+            var result = _motorcycleService.AddMotorcycle(motorcycle);
+            return Ok(result);
         }
 
         [HttpPut]
         public async Task<ActionResult<List<Motorcycle>>> UpdateHero(Motorcycle request)
         {
-            var motorcycle = motorcycleList.Find(x => x.Id == request.Id);
+            var result = _motorcycleService.UpdateHero(request);
 
-            if (motorcycle is null)
+            if (result is null)
             {
                 return NotFound("Sorry, this motorcycle doesn't exist");
             }
-
-            motorcycle.ManufacturerId = request.ManufacturerId;
-            motorcycle.Model = request.Model;
-            motorcycle.Year = request.Year;
-            motorcycle.HorsePower = request.HorsePower;
-            motorcycle.Torque = request.Torque;
-            motorcycle.EngineDisplacement = request.EngineDisplacement;
-            motorcycle.TankCapacity = request.TankCapacity;
             
-            return Ok(motorcycleList);
+            return Ok(result);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Motorcycle>> DeleteMotorcycle(int id)
         {
-            var motorcycle = motorcycleList.Find(x => x.Id == id);
+            var result = _motorcycleService.DeleteMotorcycle(id);
 
-            if (motorcycle is null)
+            if (result == null)
             {
                 return NotFound("Sorry, this motorcycle doesn't exist");
             }
 
-            motorcycleList.Remove(motorcycle);
-            return Ok(motorcycle);
+            return Ok(result);
         }
     }
 }
