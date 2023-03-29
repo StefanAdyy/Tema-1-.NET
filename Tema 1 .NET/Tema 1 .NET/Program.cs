515976f28a0fe47af62f23b198c8634df92cbbbd
StefanAdyy <stefanmorariu@rocketmail.com>
2023-03-29 21:08:54 +0300

    Added Motorcycle Service and Repository

--- Tema 1 .NET/Tema 1 .NET/Program.cs
+++ Tema 1 .NET/Tema 1 .NET/Program.cs
@@ -1,3 +1,4 @@
+using Tema_1_.NET.Repositories;
 using Tema_1_.NET.Services.MotorcycleService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<MotorcycleRepository, MotorcycleRepository>();
 builder.Services.AddScoped<IMotorcycleService, MotorcycleService>();
 
 var app = builder.Build();
